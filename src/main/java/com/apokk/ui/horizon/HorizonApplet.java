e05ed1fd554eece4fb7398b6db2365515fa6da57
Adrian Prawda <adrian.d.prawda@gmail.com>
2018-06-30 23:37:45 +0200

    implemented applet test class for debugging

--- src/main/java/com/apokk/ui/horizon/HorizonApplet.java
+++ src/main/java/com/apokk/ui/horizon/HorizonApplet.java
@@ -1,5 +1,31 @@
 package com.apokk.ui.horizon;
 
-public class HorizonApplet {
+import processing.core.PApplet;
+import processing.core.PShape;
+
+import com.apokk.ui.horizon.eicas.*;
+
+// Primarily used for testing purposes
+public class HorizonApplet extends PApplet {
     
+    public static void main(String[] args) {
+        PApplet.main("com.apokk.ui.horizon.HorizonApplet");
+    }
+
+    public void settings() {
+        size(200, 200);
+    }
+
+    public void setup() {
+        background(0);
+    }
+
+    public void draw() {
+        stroke(255);
+        background(0);
+
+        CircularDisplay s = new CircularDisplay(this, "s", 100, 100, 50, 600, 800, 999);
+        s.init();
+        s.draw();
+    }
 }
