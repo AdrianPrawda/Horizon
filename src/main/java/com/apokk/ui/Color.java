ca6159fadde27013e0115a935957c90b92881a89
Adrian Prawda <adrian.d.prawda@gmail.com>
2018-07-01 03:23:26 +0200

    fixed alpha channel bug

--- src/main/java/com/apokk/ui/Color.java
+++ src/main/java/com/apokk/ui/Color.java
@@ -5,26 +5,24 @@ public class Color {
     private int alpha;
 
     public Color(int r, int g, int b) {
-        hex = b;
-        hex += g << 8;
-        hex += r << 16;
-        alpha = 255;
+        this(r, g, b, 255);
     }
 
     public Color(int r, int g, int b, int a) {
         hex = b;
         hex += g << 8;
         hex += r << 16;
+        hex += a << 24;
         alpha = a;
     }
 
     public Color(int hex) {
-        this.hex = hex;
+        this.hex = hex + 0xff000000;
         alpha = 255;
     }
 
     public Color(int hex, int a) {
-        this.hex = hex;
+        this.hex = hex + a << 24;
         alpha = a;
     }
 
