3d50f63e6969c5bde612fcd748fa94ace3602fe5
Adrian Prawda <adrian.d.prawda@gmail.com>
2018-07-01 04:45:19 +0200

    More util stuff

--- src/main/java/com/apokk/ui/ShapeUtil.java
+++ src/main/java/com/apokk/ui/ShapeUtil.java
@@ -1,12 +1,33 @@
 package com.apokk.ui;
 
 import processing.core.PShape;
+import com.apokk.ui.Color;
 
 final public class ShapeUtil {
 
-    public void fill(int color, PShape... shapes) {
+    public static void fillAll(int color, PShape... shapes) {
         for (int i = 0; i < shapes.length; i++) {
             shapes[i].setFill(color);
+        }
+    }
+
+    public static void fillAll(Color color, PShape... shapes) {
+        ShapeUtil.fillAll(color.hex(), shapes);
+    }
+
+    public static void setStrokes(int color, PShape... shapes) {
+        for (int i = 0; i < shapes.length; i++) {
+            shapes[i].setStroke(color);
+        }
+    }
+
+    public static void setStrokes(Color color, PShape... shapes) {
+        ShapeUtil.setStrokes(color, shapes);
+    }
+
+    public static void strokeWeights(float weight, PShape... shapes) {
+        for (int i = 0; i < shapes.length; i++) {
+            shapes[i].setStrokeWeight(weight);
         }
     }
 }
