a65440054ee3a4dd4eb8f7d32502633ce7d5b846
Adrian Prawda <adrian.d.prawda@gmail.com>
2018-07-01 04:44:24 +0200

    Updated ui base interface

--- src/main/java/com/apokk/ui/UIElement.java
+++ src/main/java/com/apokk/ui/UIElement.java
@@ -4,7 +4,7 @@ public interface UIElement {
     void draw();
     void update();
     void init();
-    void target(double value);
+    void target(float value);
     void setFailureMode(int mode);
     String getID();
 }
